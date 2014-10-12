31738cd5aa5ec8ef5d3c64fe2a1a965cb5b6bb87
Mandarava <569045298@qq.com>
2014-10-12 15:47:01 +0800

    update

---
+++ 异常捕获/ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class DivisorIsZero
+    {
+        private static void Main()
+        {
+            int dividend = 10;
+            int divisor1 = 0;
+            int divisor2 = 5;
+            int DivideValue;
+
+            try
+            {
+                DivideValue = dividend / divisor1; //(1)  
+                //DivideValue=dividend/divisor2; //(2)  
+                System.Console.WriteLine("DivideValue={0}", DivideValue);//（3）这一行将不会被执行。  
+            }
+            catch(Exception e)
+            {
+                //System.Console.WriteLine("传递过来的异常值为：{0}", e);
+                DivideValue = dividend / divisor2;
+                System.Console.WriteLine("DivideValue={0}", DivideValue);//（3）这一行将不会被执行
+            }
+            finally
+            {
+                System.Console.WriteLine("无论是否发生异常，我都会显示。");
+            }
+            Console.ReadKey();
+        }
+    } 
+}
