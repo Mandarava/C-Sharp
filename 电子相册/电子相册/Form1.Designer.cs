eb7151aa4e1b78a58c8c740d2e04497bf472b239
Mandarava <569045298@qq.com>
2014-11-06 00:12:29 +0800

    update

--- 电子相册/电子相册/Form1.Designer.cs
+++ 电子相册/电子相册/Form1.Designer.cs
@@ -32,10 +32,20 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.视图ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.适应ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.拉伸ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.普通ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助HToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.查看帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // timer1
@@ -50,61 +60,149 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(29, 49);
+            this.pictureBox1.Location = new System.Drawing.Point(13, 37);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(543, 402);
+            this.pictureBox1.Size = new System.Drawing.Size(889, 548);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(595, 40);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(105, 41);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "选择图片";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(708, 40);
+            this.button2.BackColor = System.Drawing.Color.Azure;
+            this.button2.Location = new System.Drawing.Point(394, 593);
             this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(97, 41);
             this.button2.TabIndex = 2;
             this.button2.Text = "浏览";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(595, 89);
+            this.listBox1.Location = new System.Drawing.Point(50, 606);
             this.listBox1.Margin = new System.Windows.Forms.Padding(4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(210, 364);
+            this.listBox1.Size = new System.Drawing.Size(137, 49);
             this.listBox1.TabIndex = 3;
+            this.listBox1.Visible = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.视图ToolStripMenuItem,
+            this.帮助HToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(915, 30);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.ShortcutKeyDisplayString = "";
+            this.文件ToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F)));
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(69, 26);
+            this.文件ToolStripMenuItem.Text = "文件(F)";
+            // 
+            // 打开ToolStripMenuItem
+            // 
+            this.打开ToolStripMenuItem.Name = "打开ToolStripMenuItem";
+            this.打开ToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.打开ToolStripMenuItem.Size = new System.Drawing.Size(166, 24);
+            this.打开ToolStripMenuItem.Text = "打开";
+            this.打开ToolStripMenuItem.Click += new System.EventHandler(this.打开ToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(166, 24);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // 视图ToolStripMenuItem
+            // 
+            this.视图ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.适应ToolStripMenuItem,
+            this.拉伸ToolStripMenuItem,
+            this.普通ToolStripMenuItem});
+            this.视图ToolStripMenuItem.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.视图ToolStripMenuItem.Name = "视图ToolStripMenuItem";
+            this.视图ToolStripMenuItem.Padding = new System.Windows.Forms.Padding(4, 1, 4, 1);
+            this.视图ToolStripMenuItem.Size = new System.Drawing.Size(71, 26);
+            this.视图ToolStripMenuItem.Text = "视图(V)";
+            // 
+            // 适应ToolStripMenuItem
+            // 
+            this.适应ToolStripMenuItem.Name = "适应ToolStripMenuItem";
+            this.适应ToolStripMenuItem.Size = new System.Drawing.Size(148, 24);
+            this.适应ToolStripMenuItem.Text = "适应";
+            this.适应ToolStripMenuItem.Click += new System.EventHandler(this.适应ToolStripMenuItem_Click);
+            // 
+            // 拉伸ToolStripMenuItem
+            // 
+            this.拉伸ToolStripMenuItem.Checked = true;
+            this.拉伸ToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.拉伸ToolStripMenuItem.Name = "拉伸ToolStripMenuItem";
+            this.拉伸ToolStripMenuItem.Size = new System.Drawing.Size(148, 24);
+            this.拉伸ToolStripMenuItem.Text = "拉伸(建议)";
+            this.拉伸ToolStripMenuItem.Click += new System.EventHandler(this.拉伸ToolStripMenuItem_Click);
+            // 
+            // 普通ToolStripMenuItem
+            // 
+            this.普通ToolStripMenuItem.Name = "普通ToolStripMenuItem";
+            this.普通ToolStripMenuItem.Size = new System.Drawing.Size(148, 24);
+            this.普通ToolStripMenuItem.Text = "普通";
+            this.普通ToolStripMenuItem.Click += new System.EventHandler(this.普通ToolStripMenuItem_Click);
+            // 
+            // 帮助HToolStripMenuItem
+            // 
+            this.帮助HToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.查看帮助ToolStripMenuItem});
+            this.帮助HToolStripMenuItem.Name = "帮助HToolStripMenuItem";
+            this.帮助HToolStripMenuItem.Size = new System.Drawing.Size(73, 26);
+            this.帮助HToolStripMenuItem.Text = "帮助(H)";
+            // 
+            // 查看帮助ToolStripMenuItem
+            // 
+            this.查看帮助ToolStripMenuItem.Name = "查看帮助ToolStripMenuItem";
+            this.查看帮助ToolStripMenuItem.Size = new System.Drawing.Size(138, 24);
+            this.查看帮助ToolStripMenuItem.Text = "查看帮助";
+            this.查看帮助ToolStripMenuItem.Click += new System.EventHandler(this.查看帮助ToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(847, 511);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(229)))), ((int)(((byte)(242)))));
+            this.ClientSize = new System.Drawing.Size(915, 673);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "电子相册";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -116,6 +214,16 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 视图ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 适应ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 拉伸ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 普通ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助HToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 查看帮助ToolStripMenuItem;
     }
 }
 
--- 电子相册/电子相册/Form1.cs
+++ 电子相册/电子相册/Form1.cs
@@ -1,35 +1,119 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
 using System.Runtime.InteropServices;
-using System.Media;
+using System.Windows.Forms;
 
 namespace 电子相册
 {
     public partial class Form1 : Form
     {
         private int PicNo; //定义数据成员变量PicNo，表示显示图片号
-        private bool bStatus=false;
-        [DllImport("winmm.dll")]
-        public static extern bool PlaySound(string pszSound, int hmod, int fdwSound);
+        private bool bStatus;
+
         public string path = Application.StartupPath + @"\1.wav";
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        [DllImport("winmm.dll")]
+        public static extern bool PlaySound(string pszSound, int hmod, int fdwSound);
+
         private void Form1_Load(object sender, EventArgs e)
         {
             PicNo = 0;
-            timer1.Enabled = false;// 设置定时器不可用	
+            timer1.Enabled = false; // 设置定时器不可用	
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count == 0)
+            {
+                MessageBox.Show("请打开文件!");
+                openFile();
+            }
+
+            bStatus = !bStatus;
+            if (bStatus)
+            {
+                timer1.Enabled = true;
+                button2.Text = "暂停";
+                PlaySound(path, 0, 9);
+            }
+            else
+            {
+                timer1.Enabled = false;
+                button2.Text = "浏览";
+                PlaySound("0", 0, 0);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex = PicNo;
+            string s = listBox1.SelectedItem.ToString(); // 得到某一要显示图片的路径
+            pictureBox1.Image = Image.FromFile(s); // 加载图片
+            PicNo = PicNo + 1; // 为得到下一张图片做准备
+            if (PicNo >= listBox1.Items.Count)
+            {
+                // 如果是最后一张，则转为第一张
+                PicNo = 0;
+            }
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void 适应ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Size = new Size(870, 522);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            拉伸ToolStripMenuItem.Checked = false;
+            普通ToolStripMenuItem.Checked = false;
+            适应ToolStripMenuItem.Checked = true;
+            普通ToolStripMenuItem.CheckState = CheckState.Unchecked;
+            适应ToolStripMenuItem.CheckState = CheckState.Checked;
+            拉伸ToolStripMenuItem.CheckState = CheckState.Unchecked;
+        }
+
+        private void 拉伸ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Size = new Size(870, 522);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            拉伸ToolStripMenuItem.Checked = true;
+            普通ToolStripMenuItem.Checked = false;
+            适应ToolStripMenuItem.Checked = false;
+            普通ToolStripMenuItem.CheckState = CheckState.Unchecked;
+            适应ToolStripMenuItem.CheckState = CheckState.Unchecked;
+            拉伸ToolStripMenuItem.CheckState = CheckState.Checked;
+        }
+
+        private void 普通ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Size = new Size(870, 522);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+            拉伸ToolStripMenuItem.Checked = false;
+            普通ToolStripMenuItem.Checked = true;
+            适应ToolStripMenuItem.Checked = false;
+            普通ToolStripMenuItem.CheckState = CheckState.Checked;
+            适应ToolStripMenuItem.CheckState = CheckState.Unchecked;
+            拉伸ToolStripMenuItem.CheckState = CheckState.Unchecked;
+        }
+
+        private void 打开ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFile();
+        }
+
+        private void 查看帮助ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("音乐文件请置于应用程序同文件夹下，wav文件，重命名为1.wav");
+        }
+
+        private void openFile()
         {
             openFileDialog1.Multiselect = true;
             openFileDialog1.Filter = "位图文件|*.bmp|GIF文件|*.gif|JPEG文件|*.jpg|PNG文件|*.png";
@@ -40,37 +124,5 @@ namespace 电子相册
                 listBox1.Items.Add(s);
             }
         }
-
-        private void button2_Click(object sender, EventArgs e)
-        {           
-            bStatus = !bStatus;
-            if (bStatus)
-            {
-                timer1.Enabled = true;
-                button2.Text = "暂停";
-                PlaySound(path, 0, 9);
-            }
-            else
-            {                
-                timer1.Enabled = false;
-                button2.Text = "浏览";
-                PlaySound(@"G:\0.wav", 0, 9);
-            }
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            listBox1.SelectedIndex = PicNo;
-            string s = listBox1.SelectedItem.ToString();// 得到某一要显示图片的路径
-            pictureBox1.Image = Image.FromFile(s);   // 加载图片
-            PicNo = PicNo + 1;// 为得到下一张图片做准备
-            if (PicNo >= listBox1.Items.Count)
-                // 如果是最后一张，则转为第一张
-                PicNo = 0;
-        }
-
-
-
-
     }
 }
