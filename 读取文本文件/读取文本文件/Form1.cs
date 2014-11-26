8591acb6b3d39f4ef7a0fcaaa9047ac944fe77cd
Mandarava <569045298@qq.com>
2014-11-26 23:09:46 +0800

    update

---
+++ 读取文本文件/读取文本文件/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 读取文本文件
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = Application.StartupPath;
+            var sr = new StreamReader(path + @"\Form1.cs", Encoding.UTF8);
+            string str;
+            str = sr.ReadToEnd();
+            richTextBox1.Text = str;
+            sr.Close();
+        }
+    }
+}
