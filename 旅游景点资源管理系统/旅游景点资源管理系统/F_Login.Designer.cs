da4e74375fe79aefebd2dc7cac9dd62f8d1df44c
Mandarava <569045298@qq.com>
2015-01-11 14:49:59 +0800

    create project

---
+++ 旅游景点资源管理系统/旅游景点资源管理系统/F_Login.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace 旅游景点资源管理系统
+{
+    partial class F_Login
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblAccount = new System.Windows.Forms.Label();
+            this.lblPwd = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.txtAccount = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblAccount
+            // 
+            this.lblAccount.AutoSize = true;
+            this.lblAccount.Location = new System.Drawing.Point(3, 13);
+            this.lblAccount.Name = "lblAccount";
+            this.lblAccount.Size = new System.Drawing.Size(68, 15);
+            this.lblAccount.TabIndex = 0;
+            this.lblAccount.Text = "管理员ID";
+            // 
+            // lblPwd
+            // 
+            this.lblPwd.AutoSize = true;
+            this.lblPwd.Location = new System.Drawing.Point(246, 13);
+            this.lblPwd.Name = "lblPwd";
+            this.lblPwd.Size = new System.Drawing.Size(37, 15);
+            this.lblPwd.TabIndex = 1;
+            this.lblPwd.Text = "密码";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(531, 9);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "登陆";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(627, 9);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "重置";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // txtAccount
+            // 
+            this.txtAccount.BackColor = System.Drawing.SystemColors.Control;
+            this.txtAccount.Location = new System.Drawing.Point(77, 10);
+            this.txtAccount.Name = "txtAccount";
+            this.txtAccount.Size = new System.Drawing.Size(149, 25);
+            this.txtAccount.TabIndex = 4;
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.BackColor = System.Drawing.SystemColors.Control;
+            this.txtPwd.Location = new System.Drawing.Point(289, 10);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(197, 25);
+            this.txtPwd.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = global::旅游景点资源管理系统.Properties.Resources._61_140_C_500_280;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 65);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1069, 543);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(948, 642);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(55, 15);
+            this.lblTimer.TabIndex = 7;
+            this.lblTimer.Text = "label3";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.lblAccount);
+            this.panel1.Controls.Add(this.txtAccount);
+            this.panel1.Controls.Add(this.lblPwd);
+            this.panel1.Controls.Add(this.btnReset);
+            this.panel1.Controls.Add(this.txtPwd);
+            this.panel1.Controls.Add(this.btnLogin);
+            this.panel1.Location = new System.Drawing.Point(63, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(706, 36);
+            this.panel1.TabIndex = 8;
+            // 
+            // F_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1144, 678);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "F_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "旅游景点资源管理系统";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAccount;
+        private System.Windows.Forms.Label lblPwd;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox txtAccount;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ 旅游景点资源管理系统/旅游景点资源管理系统/F_Login.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 旅游景点资源管理系统
+{
+    public partial class F_Login : Form
+    {
+        private SqlConnection con;
+        private DataSet dataSet;
+        private SqlDataAdapter sqlDataAdapter;
+        private SqlDataReader sqlDataReader;
+        private SqlCommand sqlCommand;
+        public F_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblTimer.Text = DateTime.Now.ToString();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ClrText(this);
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection("Server=.;Initial Catalog=TRMS;Integrated Security=SSPI");
+            int count = 0;
+            try
+            {
+                string sql =
+                    "select COUNT(*) from account where ID='" + txtAccount.Text + "'" + "and ID_password='" +
+                    txtPwd.Text + "'";
+                con.Open();
+                sqlCommand = new SqlCommand(sql, con);               
+                count = Convert.ToInt32(sqlCommand.ExecuteScalar());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close(); 
+            }
+            if (count < 1)
+            {
+                MessageBox.Show("用户名或密码错误", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                ClrText(this);
+            }
+            else
+            {
+                MessageBox.Show("登陆成功！");
+            }
+        }
+
+        private void ClrText(Control ctrlTop)
+        {
+            if (ctrlTop.GetType() == typeof(TextBox))
+                ctrlTop.Text = "";
+            else
+            {
+                foreach (Control ctrl in ctrlTop.Controls)
+                {
+                    ClrText(ctrl); 
+                }
+            }
+        }
+    }
+}
